2a715a5152764ed7644cc57caeb5f474fefe1413
James Badger <james@jamesbadger.ca>
2013-10-10 15:22:48 -0600

    Add show view with dummy layout

--- app/controllers/workbench/sensors_controller.rb
+++ app/controllers/workbench/sensors_controller.rb
@@ -2,5 +2,8 @@ require_dependency "workbench/application_controller"
 
 module Workbench
   class SensorsController < ApplicationController
+    def show
+      @sensor = params[:id]
+    end
   end
 end
