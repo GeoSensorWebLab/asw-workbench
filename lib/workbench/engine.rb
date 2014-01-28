1dd4f3cea880f3e36032884d9b7d085ac95d52f6
James Badger <james@jamesbadger.ca>
2014-01-28 13:18:25 -0700

    Add asset precompilation for all assets

--- lib/workbench/engine.rb
+++ lib/workbench/engine.rb
@@ -9,5 +9,10 @@ require "jquery-rails"
 module Workbench
   class Engine < ::Rails::Engine
     isolate_namespace Workbench
+
+    initializer :assets do |config|
+      Rails.application.config.assets.initialize_on_precompile = true
+      Rails.application.config.assets.precompile += %w( *.js *.css *.png *.jpg *.jpeg *.gif)
+    end
   end
 end
