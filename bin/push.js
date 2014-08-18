435a3226d7088a6f9613b58b19568236fe5e27da
James Badger <james@jamesbadger.ca>
2014-08-18 10:12:05 -0600

    Add verbose logging for push script

--- bin/push.js
+++ bin/push.js
@@ -3,16 +3,25 @@ var fs = require('fs');
 var request = require('request');
 
 var argv = require('yargs')
-  .usage('Usage: $0 <input records> <destination URL>')
+  .usage('Usage: $0 [options] <input records> <destination URL>')
+  .count('verbose')
+  .boolean('verbose')
+  .alias('v', 'verbose')
   .demand(2)
   .argv;
 
+VERBOSE_LEVEL = argv.verbose;
+var file = argv._[0];
 var uploadEndpoint = argv._[1];
 
-var file = argv._[0];
+function WARN()  { VERBOSE_LEVEL >= 0 && console.log.apply(console, arguments); }
+function INFO()  { VERBOSE_LEVEL >= 1 && console.log.apply(console, arguments); }
+function DEBUG() { VERBOSE_LEVEL >= 2 && console.log.apply(console, arguments); }
+
+
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
-    console.log("Error: " + err);
+    WARN("Error: " + err);
     return;
   }
 
@@ -30,6 +39,8 @@ var uploadRecord = function (record) {
     uri: uploadEndpoint,
     json: JSON.stringify(record)
   }, function (e, r, body) {
-    console.log("Upload complete", r.statusCode);
+    WARN("Upload complete", r.statusCode);
+    INFO("Error info: ", e);
+    DEBUG("Response body:\n", body);
   });
 };
