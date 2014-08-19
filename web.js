b3ad4cc8b00870116999eda2538c67c62d8a6f8a
James Badger <james@jamesbadger.ca>
2014-08-19 10:54:23 -0600

    Add simple JS file for launching web process

---
+++ web.js
@@ -0,0 +1,30 @@
+var coffee    = require('coffee-script/register'),
+    express   = require('express'),
+    assets    = require('connect-assets'),
+    logfmt    = require('logfmt'),
+    app       = express();
+
+var port = Number(process.env.PORT || 1337);
+
+app.set('views', 'src/views');
+app.set('view engine', 'jade');
+require('./routes')(app);
+
+assets = assets({
+  paths: [
+    'src/scripts',
+    'src/images',
+    'src/stylesheets',
+    'src/views',
+    'bower_components'
+    ],
+  buildDir: 'dist',
+  gzip: true
+});
+assets.mincer.MacroProcessor.configure(['.js']);
+
+app.use(assets);
+app.use(logfmt.requestLogger());
+
+app.listen(port);
+console.log('Listening on port: ' + port);
