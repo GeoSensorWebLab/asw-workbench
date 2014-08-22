1494ee8392c4cadfb1f4dc92fc88f67ca9b22023
James Badger <james@jamesbadger.ca>
2014-08-22 09:58:57 -0600

    Add more sample stations

--- src/scripts/workbench/sample-stations.js
+++ src/scripts/workbench/sample-stations.js
@@ -38,4 +38,66 @@ window.sampleStations = [{
     },
     properties: {}
   }
+},
+{
+  id: "2",
+  name: "Finse Alpine Research Centre",
+  shortName: "FINSE",
+  _embedded: {
+    sensors: []
+  },
+  _links: {
+    site_info: {
+      description: "Finse Alpine Research Center",
+      href: "http://www.finse.uio.no/"
+    },
+    interact_info: {
+      description: "INTER-ACT: FINSE",
+      href: "http://www.eu-interact.org/field-sites/norway-2/finse/"
+    }
+  },
+  geo: {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [7.5, 60.6, 1215]
+    },
+    crs: {
+      type: "link",
+      properties: {
+        href: "http://spatialreference.org/ref/epsg/4979/proj4/",
+        type: "proj4"
+      }
+    },
+    properties: {}
+  }
+},
+{
+  id: "3",
+  name: "Litla-Skard",
+  shortName: "LITLA-SKARD",
+  _embedded: {
+    sensors: []
+  },
+  _links: {
+    interact_info: {
+      description: "INTER-ACT: Litla-Skard",
+      href: "http://www.eu-interact.org/field-sites/iceland-1/litla-skard/"
+    }
+  },
+  geo: {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-21.63, 64.726667, 115]
+    },
+    crs: {
+      type: "link",
+      properties: {
+        href: "http://spatialreference.org/ref/epsg/4979/proj4/",
+        type: "proj4"
+      }
+    },
+    properties: {}
+  }
 }];
