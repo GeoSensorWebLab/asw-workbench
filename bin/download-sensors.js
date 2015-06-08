925aff7811826d1b3a29b31cb0ffdb74147895a6
James Badger <james@jamesbadger.ca>
2015-06-08 15:55:34 -0600

    Add API key option for sensor downloader

--- bin/download-sensors.js
+++ bin/download-sensors.js
@@ -1,12 +1,14 @@
 // Download sensors from RPI Data Service
 // Outputs each sensor in list to a file in output directory.
+// If Api Key is specified, then it will be added to each object. Api Key is
+// for access through the Data Service Proxy.
 
 var fs = require('fs');
 var request = require('request');
 var qs = require('querystring');
 
 var argv = require('yargs')
-  .usage('Usage: $0 <RPI HOST URL> <User UID> <-o directory>')
+  .usage('Usage: $0 <RPI HOST URL> <User UID> [API key] <-o directory>')
   .demand(2)
   .demand(['o'])
   .argv;
@@ -25,6 +27,11 @@ request({
 var outputStations = function (stations) {
   stations.forEach(function(item) {
     var filename = argv.o + "/" + encodeURIComponent(item.title) + ".json";
+
+    if (argv._[2]) {
+      item.api_key = argv._[2];
+    }
+    
     fs.writeFile(filename, JSON.stringify(item, null, "  "), function (err) {
       if (err) {
         console.error("error!", err);
