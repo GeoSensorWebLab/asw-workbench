f8762b189a7b418996713da44e600bd7e0f9f041
James Badger <james@jamesbadger.ca>
2015-06-12 15:56:44 -0600

    Load views, styles using Broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,10 @@
+var compileSass = require('broccoli-sass');
 var funnel = require('broccoli-funnel');
+var jade = require('broccoli-jade');
+var mergeTrees = require('broccoli-merge-trees');
 
+var views = jade('src/views');
 
-var tree = funnel('src', {
-  destDir: '/'
-});
+var styles = compileSass(['src/stylesheets'], 'main.scss', 'css/main.css');
 
-module.exports = tree;
+module.exports = mergeTrees([views, styles]);
