7b557bdf7ec2f8b44ff5d2f283a13c3c078fde01
James Badger <jamesbadger@gmail.com>
2015-12-03 11:54:59 -0700

    Load JS libraries using broccoli instead of mincer

--- Brocfile.js
+++ Brocfile.js
@@ -16,6 +16,43 @@ var assetsTree = minceTree('app', {
   ]
 });
 
+// Copy scripts to output directory
+var jquery = funnel('bower_components/jquery/dist', {
+  destDir: 'scripts'
+});
+
+var jqueryTransit = funnel('bower_components/jquery.transit', {
+  destDir: 'scripts',
+  files: ['jquery.transit.js']
+});
+
+var json2 = funnel('bower_components/json2', {
+  destDir: 'scripts',
+  files: ['json2.js']
+});
+
+var jqueryDeparam = funnel('bower_components/jquery-deparam', {
+  destDir: 'scripts',
+  files: ['jquery-deparam.js']
+});
+
+var proj4 = funnel('bower_components/proj4/dist', {
+  destDir: 'scripts'
+});
+
+var proj4leaflet = funnel('bower_components/proj4leaflet/src', {
+  destDir: 'scripts'
+});
+
+var markerCluster = funnel('bower_components/leaflet.markercluster/dist', {
+  destDir: 'scripts',
+  files: ['leaflet.markercluster.js']
+});
+
+var polarmap = funnel('bower_components/polarmap/dist', {
+  destDir: 'scripts'
+});
+
 // Copy Font Awesome files to output directory
 var faFonts = funnel('bower_components/font-awesome/fonts', {
   destDir: 'fonts'
@@ -47,4 +84,6 @@ var locals = assetsHelper(assetsTree);
 var views = jade('app/views', {data: locals});
 
 module.exports = mergeTrees([assetsTree, views, faFonts, faStyles,
-  clusterStyles, polarmapStyles, bootstrapStyles]);
+  clusterStyles, polarmapStyles, bootstrapStyles,
+  jquery, json2, jqueryTransit, jqueryDeparam, proj4, proj4leaflet,
+  markerCluster, polarmap]);
